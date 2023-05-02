28d31eb45c1b7b0406aa612bc04366f218d4bc8a
Anesushangwa <sha21032@byui.edu>
2023-05-02 09:44:22 +0200

    new

--- index.js
+++ index.js
@@ -1,78 +1,117 @@
-// let firstcard = 6
-
-// let secondcard = 15
-
-// let sum = firstcard + secondcard
-
+// // 2. Use getRandomCard() to set the values of firstCard and secondCard
+// let firstCard = getRandomCard()
+// let secondCard = getRandomCard()
+// let cards = [firstCard, secondCard]
+// let sum = firstCard + secondCard
 // let hasBlackJack = false
+// let isAlive = true
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
 
-// let isAlive  = true
-
-// let massage = ""
-
-// let outp = document.getElementById("message-el")
-
-
-
-
-// function startGame(){
-
-  
-
-//     if (sum < 20){
-//         massage = ("Do you want to draw a new card? 🙂")
-        
-//     }
-//     else if (sum === 21){
-//         massage = ("Wohoo! You've got Blackjack! 🥳")
-//         hasBlackJack = true
-//     }else {
-//         massage = ("You're out of the game! 😭")
-//         isAlive = false
-//     }
-    
-//     // console.log(hasBlackJack)
-//     // console.log(isAlive)
-//     outp.textContent = mssage
-//     // console.log(massage)
-
+// // 1. Create a function, getRandomCard(), that always returns the number 5
+// function getRandomCard() {
+//     return 5
 // }
 
-// startGame()
+
+// function startGame() {
+//     renderGame()
+// }
+
+// function renderGame() {
+//     cardsEl.textContent = "Cards: "
+//     for (let i = 0; i < cards.length; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+    
+//     sumEl.textContent = "Sum: " + sum
+//     if (sum <= 20) {
+//         message = "Do you want to draw a new card?"
+//     } else if (sum === 21) {
+//         message = "You've got Blackjack!"
+//         hasBlackJack = true
+//     } else {
+//         message = "You're out of the game!"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
 
 
+// function newCard() {
+//     // 3. Use the getRandomCard() to set the value of card
+//     let card = getRandomCard()
+//     sum += card
+//     cards.push(card)
+//     console.log(cards)
+//     renderGame()
+// }
 
+
+let player = {
+    name: "Per",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
-    let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
- 
-// 1. Store the message-el paragraph in a variable called messageEl
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
 
-let messageEl = document.getElementById("message-el")
-
-// let  sumEl  = document.getElementById("sum-el")
-
-let  sumEl  = document.querySelector("#sum-el")
-
-let cardsEl = document.querySelector("#cards-el")
-
-cardsEl.textContent = "cards: " + firstCard +" " + secondCard
-sumEl.textContent =  "Sum::" + sum 
-
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? "
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! "
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! "
+        message = "You're out of the game!"
         isAlive = false
     }
-    // 2. Display the message in the messageEl using messageEl.textContent
     messageEl.textContent = message
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
